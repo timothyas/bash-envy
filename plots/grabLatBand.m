c40119869ec530181902b148dcc54f8bb93e4f1a
timothys-thorne <timsmith204@utexas.edu>
2016-07-06 13:22:05 -0500

    Plots: Modified grabLatBand to work with how masking is actually done (rotated faces etc.)

--- plots/grabLatBand.m
+++ plots/grabLatBand.m
@@ -1,11 +1,12 @@
-function[latVal] = grabLatBand(latMsk, gcmfld)
+function[latVal] = grabLatBand(lineMsk, ufld, vfld)
 %% Produces a 2d field of values along a latitude mask
 % Restriction: can only work in the Atlantic 
 % or can only involve gcmfaces 1 & 5
 % Inputs: 
-%   latMsk : type=gcmfaces, 2D surface
-%            contains 1's for contributing grid cells
-%   gcmfld : type=gcmfaces, 3D field
+%   lineMsk : type= field with gcmfaces objects mskWedge & mskSedge
+%            should be from LAT_MASKS or created by gcmfaces_lines_transp()
+%
+%   ufld/vfld : type=gcmfaces, 3D field
 %            contains values desired for midsection returned field
 %
 % Outputs: 
@@ -13,30 +14,58 @@ function[latVal] = grabLatBand(latMsk, gcmfld)
 %            dims: [Nx, Nr] Nx: longitudinal basin width, Nr: depth
 %-----------------------------------------------------------------------
 
+mskW = convert2array(lineMsk.mskWedge);
+mskS = convert2array(lineMsk.mskSedge); 
 
-latMat = convert2gcmfaces(latMsk);
+mskW(isnan(mskW))=0;
+mskS(isnan(mskS))=0;
 
-latInd = find(latMat == 1);
-Nx = length(latInd);
-Nr = size(gcmfld.f1,3);
+latIndW = find(mskW~=0);
+latIndS = find(mskS~=0);
+
+Nx = length(latIndW)+length(latIndS);
+Nr = size(ufld.f1,3);
 
 latVal = zeros(Nx,Nr);
 
 %% First for gcmface 5
-NxFace = zeros(2,1);
+NxFaceW = zeros(5,1);
+NxFaceS = zeros(5,1);
 for iFace = 5:-4:1
-    latMat = latMsk{iFace};
-    latInd = find(latMat == 1);
-    NxFace(iFace) = length(latInd);
+    
+    mskW = lineMsk.mskWedge{iFace};
+    mskS = lineMsk.mskSedge{iFace};
+    
+    mskW(isnan(mskW))=0;
+    mskS(isnan(mskS))=0;
+    
+    latIndW = find(mskW~=0);
+    latIndS = find(mskS~=0);
+
+    NxFaceW(iFace)=length(latIndW);
+    NxFaceS(iFace)=length(latIndS);
+    
 for k = 1:Nr
+    %% West component
     if iFace == 5
         bigInd = 1;
     else
-        bigInd = 1+NxFace(5);
+        bigInd = 1+NxFaceW(5);
     end
-    [li,lj] = ind2sub(size(latMat),latInd);
+    [li,lj] = ind2sub(size(mskW),latIndW);
     for i = 1:length(li)
-        latVal(bigInd,k) = gcmfld{iFace}(li(i),lj(i),k);
+        latVal(bigInd,k) = ufld{iFace}(li(i),lj(i),k).*mskW(li(i),lj(i));
+        bigInd = bigInd + 1;
+    end
+    %% South component
+    if iFace == 5
+        bigInd = 1;
+    else
+        bigInd = 1+NxFaceW(5);
+    end
+    [li,lj] = ind2sub(size(mskS),latIndS);
+    for i = 1:length(li)
+        latVal(bigInd,k) = vfld{iFace}(li(i),lj(i),k).*mskS(li(i),lj(i));
         bigInd = bigInd + 1;
     end
 end
