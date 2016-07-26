9c3810c201487a6c8dd3dcb503d86baa2d68b3b7
timothys-mac <timsmith204@utexas.edu>
2016-07-25 21:59:05 -0500

    plotting adj sens

--- plots/plotAdjVideo.m
+++ plots/plotAdjVideo.m
@@ -70,12 +70,17 @@ end
 
 % Prepare ranges
 vv=.25:.25:1;
-colscale = [10^-2*vv 10^-1*vv 1*vv];
+colscale = [10^-3*vv 10^-2*vv 10^-1*vv 1*vv];
 ctick = [-colscale(end:-1:1), colscale];
 Ntick = length(ctick); 
-colbarticks = [-1:8/Ntick:-8/Ntick, 0, 8/Ntick:8/Ntick:1];
-% colbarticks = [-1:8/33:-17/33 0-1/33-8/33 0 0+1/33+8/33 17/33:8/33:1];
-colbarlbl = {'-1', '-0.1', '-0.01', '0' , '0.01', '0.1', '1'};%*10^-caxLim;
+
+if Ntick==32
+    colbarticks = [-1:8/Ntick:-8/Ntick 0 8/Ntick:8/Ntick:1];
+    colbarlbl = [-1 -.1 -.01 -.001 0 .001 .01 .1 1];
+else
+    colbarticks = [-1:8/Ntick:-8/Ntick, 0, 8/Ntick:8/Ntick:1];
+    colbarlbl = [-1, -0.1, -0.01, 0 , 0.01, 0.1, 1];%*10^-caxLim;
+end
 fld=convert2gcmfaces(fld)*10^caxLim;
 binFld = fld;
 for i = 1:Ntick
