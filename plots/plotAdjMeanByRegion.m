a96fa662da7ff3e8655c17db21a833cb5715ff92
timothys-mac <timsmith204@utexas.edu>
2016-10-03 14:50:05 -0500

    plotting adj by region works now

--- plots/plotAdjMeanByRegion.m
+++ plots/plotAdjMeanByRegion.m
@@ -8,16 +8,16 @@ function [] = plotAdjMeanByRegion(dirs,mygrid,deseasonFlag,saveFig)
 %       deseasonFlag : 1 = take out seasonal signal, 0 = don't
 % -------------------------------------------------------------------------
 
-if nargin<2, dirs=establish_samocDirs; end
-if nargin<3, establish_mygrid; end
-if nargin<4, deseasonFlag=0; end
-if nargin<5, saveFig=0; end
+if nargin<1, dirs=establish_samocDirs; end
+if nargin<2, establish_mygrid; end
+if nargin<3, deseasonFlag=0; end
+if nargin<4, saveFig=0; end
 runStr = 'region-mean/';
 if ~exist([dirs.mat runStr],'dir'), mkdir([dirs.mat runStr]); end
 if ~exist([dirs.figs runStr],'dir'), mkdir([dirs.figs runStr]); end
 
-adjField = {'hflux','sflux'}; %'tauu','tauv','aqh','atemp','swdown','lwdown','precip','runoff','hflux','sflux'};
-monthStr = {'jan','feb','march','april','may','june','july','aug','sept','oct','nov','dec'};
+adjField = {'hflux','sflux','tauu','tauv'}; %,'aqh','atemp','swdown','lwdown','precip','runoff','hflux','sflux'};
+monthStr = {'june','dec'}; %{'jan','feb','march','april','may','june','july','aug','sept','oct','nov','dec'};
 Nadj = length(adjField);
 Nmo = length(monthStr);
 
@@ -26,14 +26,15 @@ Nmo = length(monthStr);
 
 
 for i = 1:Nadj
-    adjMean = zeros(Nmo,240);
-    adjMeanAcc=zeros(Nmo,240);
-    adjMeanArc=zeros(Nmo,240);
-    adjMeanNorth=zeros(Nmo,240);
-    adjMeanInd=zeros(Nmo,240);
-    adjMeanAtl=zeros(Nmo,240);
-    adjMeanPac=zeros(Nmo,240);
-    for m = 6:6:Nmo
+    Nt = 241;
+    adjMean = zeros(Nmo,Nt);
+    adjMeanAcc=zeros(Nmo,Nt);
+    adjMeanArc=zeros(Nmo,Nt);
+    adjMeanNorth=zeros(Nmo,Nt);
+    adjMeanInd=zeros(Nmo,Nt);
+    adjMeanAtl=zeros(Nmo,Nt);
+    adjMeanPac=zeros(Nmo,Nt);
+    for m = 1:Nmo
         adjFile = sprintf('%s%s.240mo/adj_%s.mat',dirs.mat,monthStr{m},adjField{i});
         if exist(adjFile,'file')
             load(adjFile);
@@ -67,7 +68,6 @@ for i = 1:Nadj
             % Pacific from 60S
             adjMeanPac(m,:) = computeRegionMean(adxx,msks.pac,mygrid);
             if deseasonFlag, adjMeanPac=removeSeasonality(adjMeanPac); end
-            
         end
     end
 
@@ -79,56 +79,66 @@ for i = 1:Nadj
     subplot(4,2,1),plot(t, adjMean)    
     if strcmp(adjField{i},'tauu') || strcmp(adjField{i},'tauv')
         xlim([Nt-36 Nt])
+    else xlim([1 Nt])
     end
     ylabel('Spatial Mean ( dJ/du(t) )')
-    title(sprintf('Mean of %s sens. (global)',adjField{i}))
+    title('Global')
     
     % Arctic
     subplot(4,2,2),plot(t,adjMeanArc )
     if strcmp(adjField{i},'tauu') || strcmp(adjField{i},'tauv')
         xlim([Nt-36 Nt])
+    else xlim([1 Nt])
     end
-    title(sprintf('Mean of %s sens. (Arctic)',adjField{i}))
+    title('Arctic')
 
     % North Box 
     subplot(4,2,3),plot(t,adjMeanNorth)
     if strcmp(adjField{i},'tauu') || strcmp(adjField{i},'tauv')
         xlim([Nt-36 Nt])
+    else xlim([1 Nt])
     end
-    ylabel('Spatial Mean ( dJ/du(t) )')
-    title(sprintf('Mean of %s sens. (Greenland)',adjField{i}))
+    title('Greenland')
 
 
     % Atlantic
     subplot(4,2,4), plot(t,adjMeanAtl)
     if strcmp(adjField{i},'tauu') || strcmp(adjField{i},'tauv')
         xlim([Nt-36 Nt])
+    else xlim([1 Nt])
     end
-    title(sprintf('Mean of %s sens. (Atlantic)',adjField{i}))
+    title('Atlantic')
 
     % Pacific
     subplot(4,2,5), plot(t,adjMeanPac)
     if strcmp(adjField{i},'tauu') || strcmp(adjField{i},'tauv')
         xlim([Nt-36 Nt])
+    else xlim([1 Nt])
     end
     ylabel('Spatial Mean ( dJ/du(t) )')
-    title(sprintf('Mean of %s sens. (Pacific)',adjField{i}))
+    title('Pacific')
 
     % Indian
     subplot(4,2,6), plot(t,adjMeanInd)
     if strcmp(adjField{i},'tauu') || strcmp(adjField{i},'tauv')
         xlim([Nt-36 Nt])
+    else xlim([1 Nt])
     end
-    title(sprintf('Mean of %s sens. (Indian)',adjField{i}))
+    title('Indian')
 
     % ACC
     subplot(4,2,7), plot(t,adjMeanAcc)
     if strcmp(adjField{i},'tauu') || strcmp(adjField{i},'tauv')
         xlim([Nt-36 Nt])
+    else xlim([1 Nt])
     end
     xlabel('Months')
-    ylabel('Spatial Mean ( dJ/du(t) )')
-    title(sprintf('Mean of %s sens. (ACC)',adjField{i}))
+    title('ACC')
+    
+    % Text & Legend in bottom right
+    subplot(4,2,8), plot(1,1,2,2), axis off
+    legend('June dJ/dF','Dec. dJ/dF','Location','West')
+    
 
     set(gcf,'paperorientation','portrait')
     set(gcf,'paperunits','normalized')
