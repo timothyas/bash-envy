0f6e2ea7f807787bfa8982107615071b09cba1db
timothys-thorne <timsmith204@utexas.edu>
2016-10-06 16:59:57 -0500

    plotting original sign then done

--- ocean-modeling/tutorial_idealized_atlantic/plotIdealizedAtl.m
+++ ocean-modeling/tutorial_idealized_atlantic/plotIdealizedAtl.m
@@ -1,6 +1,6 @@
 %Not necessary on thorneaddpath ../../../MITgcmutils/matlab
 runDir = '../../../MITgcm/mysetups/tutorial_idealized_atlantic/run_eq/';
-plotDir = 'flip-tau/'
+plotDir = 'orig-tau/'
 if ~exist(plotDir,'dir'),mkdir(plotDir);end;
 
 
