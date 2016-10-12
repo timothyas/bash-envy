8ac627929c386e392e20d960719eb66bbfb3182b
timothys-thorne <timsmith204@utexas.edu>
2016-10-12 10:23:48 -0500

    plotting scripts for newest hw reqs

--- ocean-modeling/tutorial_idealized_atlantic/flipTauSign.m
+++ ocean-modeling/tutorial_idealized_atlantic/flipTauSign.m
@@ -5,17 +5,17 @@ inputDir = [setupDir 'input_eq/'];
 windFile = [inputDir 'windx_12x28x7.bin'];
 gridSize = [12 28];
 windx = readbin(windFile,gridSize,1,'real*8'); 
-xc=rdmds([inputDir '../run_eq/XC']);
-yc=rdmds([inputDir '../run_eq/YC']);
+xc=rdmds([inputDir '../run_orig/XC']);
+yc=rdmds([inputDir '../run_orig/YC']);
 
 %% Plot original binary file
 figure
-contourf(xc,yc,windx);
+contourf(xc,yc,windx); shading flat; colorbar;
 title('Original Horizontal Wind Field')
 set(gcf,'paperorientation','landscape')
 set(gcf,'paperunits','normalized')
 set(gcf,'paperposition',[0 0 1 1])
-saveas(gcf,'orig-wind','pdf')
+saveas(gcf,'orig-tau/orig-wind','pdf')
 
 %% Flip the sign and save
 windx=-windx;
@@ -24,9 +24,22 @@ writebin(newWindFile,windx,1,'real*8');
 
 %% Plot the flipped version
 figure
-contourf(xc,yc,windx);
+contourf(xc,yc,windx); shading flat; colorbar
 title('Flipped Horizontal Wind Field')
 set(gcf,'paperorientation','landscape')
 set(gcf,'paperunits','normalized')
 set(gcf,'paperposition',[0 0 1 1])
-saveas(gcf,'neg-wind','pdf')
+saveas(gcf,'flip-tau/flip-wind','pdf')
+
+%% Now multiply by 4, save and plot
+windx=4*windx;
+newWindFile = [inputDir 'windx_flip_4_12x28x7.bin'];
+writebin(newWindFile,windx,1,'real*8');
+
+figure
+contourf(xc,yc,windx); shading flat; colorbar
+title('4 x Flipped Horizontal Wind Field')
+set(gcf,'paperorientation','landscape')
+set(gcf,'paperunits','normalized')
+set(gcf,'paperposition',[0 0 1 1])
+saveas(gcf,'flip-4-tau/flip-4-wind','pdf')
