a133de37e20a9be1bc657e9c15528363210d180c
timothys-thorne <timsmith204@utexas.edu>
2016-10-13 15:38:55 -0500

    plotting done for homework

--- ocean-modeling/tutorial_idealized_atlantic/plotIdealizedAtl.m
+++ ocean-modeling/tutorial_idealized_atlantic/plotIdealizedAtl.m
@@ -68,45 +68,64 @@ u = rdmds([runDir 'U'],Inf);
 v = rdmds([runDir 'V'],Inf); 
 w = rdmds([runDir 'W'],Inf);
 eta=rdmds([runDir 'Eta'],Inf);
+inputDir = [runDir '../input_eq/'];
+if ~isempty(strfind(runDir,'flip4'))
+	windFile = [inputDir 'windx_flip_4_12x28x7.bin'];
+elseif ~isempty(strfind(runDir,'flip'))
+	windFile = [inputDir 'windx_flip_12x28x7.bin'];
+else
+	windFile = [inputDir 'windx_12x28x7.bin'];
+end
+gridSize = [12 28];
+windx = readbin(windFile,gridSize,1,'real*8'); 
 
 figureW;
 subplot(2,4,1),contourf(xc,yc,T(:,:,1))
+shading flat;
 title('T 20m')
 colorbar
 
 subplot(2,4,5),contourf(xc,yc,T(:,:,5))
+shading flat;
 title('T 1350m')
 colorbar
 
 sc=34:.1:36;
 subplot(2,4,2),contourf(xc,yc,S(:,:,1),sc)
+shading flat;
 title('S 20m')
 colorbar
 
 subplot(2,4,6),contourf(xc,yc,S(:,:,5),sc)
+shading flat;
 title('S 1350m')
 colorbar
 
-subplot(2,4,3),contourf(xc,yc,divergence(xc,yc,u(:,:,1),v(:,:,1)))
+subplot(2,4,3),contourf(xc,yc,w(:,:,1))
+shading flat
 hold on
-quiver(xc,yc,u(:,:,1),v(:,:,1))
+quiver(xc,yc,u(:,:,1),v(:,:,1),'k')
 hold off 
-title('(U,V) 20m')
+title(sprintf('(U,V): Arrows\nW: Contours 20m'))
 colorbar
 
-subplot(2,4,7),contourf(xc,yc,divergence(xc,yc,u(:,:,5),v(:,:,5)))
+subplot(2,4,7),contourf(xc,yc,w(:,:,5))
 hold on
-quiver(xc,yc,u(:,:,5),v(:,:,5))
+quiver(xc,yc,u(:,:,5),v(:,:,5),'k')
 hold off 
-title('(U,V) 1350m')
+title(sprintf('(U,V): Arrows\nW: Contours 1350m'))
 colorbar
 
-subplot(2,4,4),contourf(xc,yc,w(:,:,1))
-title('W 20m')
+subplot(2,4,4), %contourf(xc,yc,w(:,:,1))
+contourf(xc,yc,eta)
+shading flat;
+title('Eta')
 colorbar
 
-subplot(2,4,8),contourf(xc,yc,w(:,:,5))
-title('W 1350m')
+subplot(2,4,8),% contourf(xc,yc,w(:,:,5))
+contourf(xc,yc,windx)
+shading flat;
+title('U_{wind}')
 colorbar
 
 set(gcf,'paperorientation','landscape')
@@ -116,14 +135,15 @@ saveas(gcf,[plotDir 'vol_snapshots'],'pdf')
 close;
 
 
-figure;
-contourf(xc,yc,eta)
-colorbar
-set(gcf,'paperorientation','landscape')
-set(gcf,'paperunits','normalized')
-set(gcf,'paperposition',[0 0 1 1])
-xlabel('Longitude (Deg)')
-ylabel('Latitude (Deg)')
-title('SSH Snapshot')
-saveas(gcf,[plotDir 'EtaSnap'],'pdf')
-close;
+%figure;
+%contourf(xc,yc,eta)
+%shading flat;
+%colorbar
+%set(gcf,'paperorientation','landscape')
+%set(gcf,'paperunits','normalized')
+%set(gcf,'paperposition',[0 0 1 1])
+%xlabel('Longitude (Deg)')
+%ylabel('Latitude (Deg)')
+%title('SSH Snapshot')
+%saveas(gcf,[plotDir 'EtaSnap'],'pdf')
+%close;
